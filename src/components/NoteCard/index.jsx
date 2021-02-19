e414c50a59f61b78e13bb64bdf667b7111a40a9f
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-02-18 23:35:34 -0300

    task: add remove icon in card note

--- src/components/NoteCard/index.jsx
+++ src/components/NoteCard/index.jsx
@@ -1,10 +1,13 @@
 import './styles.css';
 
+import deleteSVG from '../../assets/images/delete.svg';
+
 function NoteCard() {
   return (
     <section className="card-nota">
       <header className="card-nota_cabecalho">
         <h3 className="card-nota_titulo">Título</h3>
+        <img src={deleteSVG} alt="Excluir nota" class="card-nota_icon" />
       </header>
       <p className="card-nota_texto">Escreva sua nota</p>
     </section>
